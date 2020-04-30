e0c46fb2e52e95f68023e8ba37b7976efeda1d72
FelipeAlves99 <falves99.dev@gmail.com>
2020-04-29 21:41:37 -0300

    Created possible forms to be used

---
+++ src/APS.ClientWindows/Views/frmServerConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APS.ClientWindows.Views
+{
+    public partial class frmServerConfig : Form
+    {
+        public frmServerConfig()
+        {
+            InitializeComponent();
+        }
+    }
+}
