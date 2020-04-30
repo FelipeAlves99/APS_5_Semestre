36070c4103abc833d41d22c603ffae637ba30eb9
FelipeAlves99 <falves99.dev@gmail.com>
2020-04-29 21:42:28 -0300

    added the server code for console app

---
+++ APS.Server/server.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace APS.Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 8000);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Bind(localEndPoint);
+            socket.Listen(10);
+            Socket client = socket.Accept();
+        }                
+    }
+}
